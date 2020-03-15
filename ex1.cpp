aa165477b82859f06184bcfee74588b643dc845e
Pacharoth <pacharothroeun01@gmail.com>
2020-03-15 15:03:02 +0700

    link list

--- ex1.cpp
+++ ex1.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+using namespace std;
+
 struct Element{
     int data;
     Element *next;
@@ -17,7 +19,26 @@ List *createList(){
     ls->n =0;
     ls->head=NULL;
     ls->tail=NULL;
+    return ls;
+
 }
+//create function to insert all number to begin
+void changeToBegin(List *ls,int newData);
+//create display list
+void displayList(List *l);
+//add to end list
+void endOfList(List *ls,int dataNew);
+int main(){
+    List *l;
+    l =createList();
+    changeToBegin(l,7);
+    changeToBegin(l,1);
+    displayList(l);
+    endOfList(l,0);
+    endOfList(l,4);
+    displayList(l);
+
+}   
 //create function to insert all number to begin
 void changeToBegin(List *ls,int newData){
     Element *e;
@@ -37,8 +58,37 @@ void changeToBegin(List *ls,int newData){
     ls->n=ls->n+1;//erach that algorithm repeat and store as singled link step by step
 
 }
+//Display list
+void displayList(List *l){
+    Element *tmp;
+    tmp =l->head;
 
+    while (tmp!=NULL)//run to null 
+    {
+        //after display to data which store value 
+        // it will go to next element to display next element
+        cout<<tmp->data<<" ";
+        tmp = tmp->next;
+    }
+    cout<<endl;
 
-int main(){
-    
+}
+//add to end ofo  list
+void endOfList(List *ls,int dataNew){
+    Element *e;
+    if (ls->n==0)
+    {
+        changeToBegin(ls,dataNew);
+    }else
+    {
+        e=new Element(); //create element
+        e->data = dataNew;//equal data
+        e->next = NULL;//next of element set null
+
+       ls->tail->next=e;//list tail go next until null
+       ls->tail = e;//the way it run to tail of list so the value of end is tail of list
+       ls->n=ls->n+1;
+
+    }
+   
 }
